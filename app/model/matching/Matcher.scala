27d49bc6478dbfc85bda761c0965d3f3aec2390a
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2022-06-08 12:00:31 +0200

    small refactoring

--- app/model/matching/Matcher.scala
+++ app/model/matching/Matcher.scala
@@ -2,6 +2,17 @@ package model.matching
 
 import scala.annotation.tailrec
 
+trait Match[T] {
+  val sampleValue: T
+  val userValue: T
+}
+
+trait MatchingResult[T, M <: Match[T]] {
+  val matches: Seq[M]
+  val notMatchedSample: Seq[T]
+  val notMatchedUser: Seq[T]
+}
+
 trait Matcher[T, M <: Match[T], MR <: MatchingResult[T, M]] {
 
   protected def matches(sampleValue: T, userValue: T): Boolean
@@ -10,10 +21,7 @@ trait Matcher[T, M <: Match[T], MR <: MatchingResult[T, M]] {
 
   protected def createMatchingResult(matches: Seq[M], notMatchedSample: Seq[T], notMatchedUser: Seq[T]): MR
 
-  protected def findUserSolutionMatch(
-    sampleSolutionNode: T,
-    userSolution: List[T]
-  ): Option[(T, List[T])] = {
+  protected def findUserSolutionMatch(sampleSolutionNode: T, userSolution: List[T]): Option[(T, List[T])] = {
 
     @tailrec
     def go(remainingUserNodes: List[T], checkedUserNodes: List[T]): Option[(T, List[T])] = remainingUserNodes match {
@@ -32,15 +40,14 @@ trait Matcher[T, M <: Match[T], MR <: MatchingResult[T, M]] {
   def performMatching(sampleSolution: Seq[T], userSolution: Seq[T]): MR = {
 
     @tailrec
-    def go(remainingSampleNodes: List[T], remainingUserNodes: List[T], matches: Seq[M], notMatchedSample: Seq[T]): MR =
-      remainingSampleNodes match {
-        case Nil => createMatchingResult(matches, notMatchedSample, remainingUserNodes)
-        case head :: tail =>
-          findUserSolutionMatch(head, remainingUserNodes) match {
-            case None                                    => go(tail, remainingUserNodes, matches, notMatchedSample :+ head)
-            case Some((userNode, newRemainingUserNodes)) => go(tail, newRemainingUserNodes, matches :+ createMatch(head, userNode), notMatchedSample)
-          }
-      }
+    def go(remainingSampleNodes: List[T], remainingUserNodes: List[T], matches: Seq[M], notMatchedSample: Seq[T]): MR = remainingSampleNodes match {
+      case Nil => createMatchingResult(matches, notMatchedSample, remainingUserNodes)
+      case head :: tail =>
+        findUserSolutionMatch(head, remainingUserNodes) match {
+          case None                                    => go(tail, remainingUserNodes, matches, notMatchedSample :+ head)
+          case Some((userNode, newRemainingUserNodes)) => go(tail, newRemainingUserNodes, matches :+ createMatch(head, userNode), notMatchedSample)
+        }
+    }
 
     go(sampleSolution.toList, userSolution.toList, Seq.empty, Seq.empty)
   }
--- app/model/matching/NodeMatcher.scala
+++ app/model/matching/NodeMatcher.scala
@@ -1,9 +1,7 @@
 package model.matching
 
-import model.{FlatSolutionEntry, SolutionEntry}
-import scala.annotation.tailrec
-import play.api.libs.json.{OFormat, Json, Format}
-import model.SolutionEntrySubText
+import model.{SolutionEntry, SolutionEntrySubText}
+import play.api.libs.json.{Format, Json, OFormat}
 
 final case class NodeMatch(
   sampleValue: SolutionEntry,
@@ -25,35 +23,30 @@ object NodeMatcher extends Matcher[SolutionEntry, NodeMatch, NodeMatchingResult]
   override protected def createMatch(sampleValue: SolutionEntry, userValue: SolutionEntry): NodeMatch =
     NodeMatch(sampleValue, userValue, performMatching(sampleValue.children, userValue.children))
 
-  override protected def createMatchingResult(matches: Seq[NodeMatch], notMatchedSample: Seq[SolutionEntry], notMatchedUser: Seq[SolutionEntry]) =
-    NodeMatchingResult(matches, notMatchedSample, notMatchedUser)
+  override protected def createMatchingResult(
+    matches: Seq[NodeMatch],
+    notMatchedSample: Seq[SolutionEntry],
+    notMatchedUser: Seq[SolutionEntry]
+  ): NodeMatchingResult = NodeMatchingResult(matches, notMatchedSample, notMatchedUser)
 
   private val solutionEntryFormat: OFormat[SolutionEntry] = {
-
     implicit val solutionEntrySubTextFormat: OFormat[SolutionEntrySubText] = Json.format
 
     Json.format
-
   }
 
   private val nodeMatchFormat: OFormat[NodeMatch] = {
-
-    implicit val x0: OFormat[SolutionEntry] = solutionEntryFormat
-
+    implicit val x0: OFormat[SolutionEntry]          = solutionEntryFormat
     implicit lazy val x1: Format[NodeMatchingResult] = nodeMatchingResultFormat
 
     Json.format[NodeMatch]
-
   }
 
   val nodeMatchingResultFormat: OFormat[NodeMatchingResult] = {
-
     implicit lazy val x0: OFormat[NodeMatch] = nodeMatchFormat
-
-    implicit val x1: OFormat[SolutionEntry] = solutionEntryFormat
+    implicit val x1: OFormat[SolutionEntry]  = solutionEntryFormat
 
     Json.format
-
   }
 
 }
