1f57d92c9a3bef8f7af90a574d7d8b8656b9c281
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2022-09-12 09:28:21 +0200

    split db in user & admin

--- init.sql
+++ init.sql
@@ -1,5 +1,11 @@
+drop database if exists coras;
 create database coras;
 
+drop user if exists coras_admin;
+drop user if exists coras;
+
+create user coras_admin with password '1234';
 create user coras with password '1234';
 
-grant all privileges on database coras to coras;
+grant all privileges on database coras to coras_admin;
+grant connect on database coras to coras;
