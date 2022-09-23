f30c3f6465c9d324d102616eded95152b27cfb5f
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2022-09-23 10:00:45 +0200

    remove old client matching code

--- ui/packages/matching/dist/certainMatching.d.ts
+++
@@ -1,4 +0,0 @@
-import { MatchingResult } from './matchingResult';
-export declare type MatchFunc<T> = (sampleValue: T, userValue: T) => boolean;
-export declare function findCertainMatches<T>(sampleValues: T[], userValues: T[], checkFunc: MatchFunc<T>): MatchingResult<T>;
-//# sourceMappingURL=certainMatching.d.ts.map
--- ui/packages/matching/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { AmbiguousMatchFunc, findAmbiguousMatches } from './ambiguousMatching';
-export { combinedMatching, CombinedMatchingResult, combinedMatchingResultQuality } from './combinedMatching';
-export { MatchFunc, findCertainMatches } from './certainMatching';
-export { AmbiguousAssessment, AmbiguousMatch, AmbiguousMatchingResult, ambiguousMatchingResultQuality, MatchingResult, certainMatchingResultQuality, Match } from './matchingResult';
-//# sourceMappingURL=index.d.ts.map
