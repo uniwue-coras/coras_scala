930d2b97aaf63c4935ff73a87779b17a509d3cff
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2023-05-30 11:55:02 +0200

    refactor

---
+++ ui/src/exercise/UserSolutionOverviewBox.tsx
@@ -0,0 +1,40 @@
+import {CorrectionStatus} from '../graphql';
+import {Link} from 'react-router-dom';
+import {JSX} from 'react';
+import {useTranslation} from 'react-i18next';
+
+interface IProps {
+  username: string;
+  exerciseId: number;
+  reviewUuid: string;
+  correctionStatus: CorrectionStatus;
+  onInitiateCorrection: () => void;
+}
+
+export function UserSolutionOverviewBox({username, exerciseId, reviewUuid, correctionStatus, onInitiateCorrection}: IProps): JSX.Element {
+
+  const {t} = useTranslation('common');
+
+  const footerElement: JSX.Element = {
+    [CorrectionStatus.Waiting]:
+      <button type="button" onClick={onInitiateCorrection}>{t('initiateCorrection')}</button>,
+    [CorrectionStatus.Ongoing]:
+      <Link className="text-blue-600" to={`/exercises/${exerciseId}/solutions/${username}/correctSolution`}>{t('correctSolution')}</Link>,
+    [CorrectionStatus.Finished]: (
+      <>
+        <div className="italic text-cyan-500">{t('correctionAlreadyFinished!')}</div>
+        <Link to={`/correctionReview/${reviewUuid}`}>{t('userView')}</Link>
+      </>
+    )
+  }[correctionStatus];
+
+  return (
+    <div key={username}>
+      <header className="p-2 rounded-t border border-slate-600 text-center">{username}</header>
+
+      <footer className="p-2 border-b border-x border-slate-600 text-center">{footerElement}</footer>
+
+    </div>
+  );
+
+}
