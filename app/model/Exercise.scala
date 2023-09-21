c04397c5891e52ec50a730bc79a41237613ad26c
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2023-09-21 13:06:06 +0200

    small refactor

--- app/model/Exercise.scala
+++ app/model/Exercise.scala
@@ -1,7 +1,12 @@
 package model
 
 import de.uniwue.ls6.model.ExportedExercise
+import model.graphql.GraphQLArguments.{userSolutionInputArg, usernameArg}
+import model.graphql._
+import sangria.macros.derive.{AddFields, deriveInputObjectType, deriveObjectType}
+import sangria.schema.{BooleanType, Field, InputObjectType, ListType, ObjectType, OptionType, fields}
 
+import scala.annotation.unused
 import scala.concurrent.{ExecutionContext, Future}
 
 final case class ExerciseInput(
@@ -26,6 +31,55 @@ final case class Exercise(
     exportedUserSolutions <- Future.traverse(userSolutionNodes) { _.exportData(tableDefs) }
 
   } yield ExportedExercise(id, title, text, exportedSampleSolutionNodes, exportedUserSolutions)
+
+}
+
+object ExerciseGraphQLTypes extends QueryType[Exercise] with MutationType[Exercise] with MyInputType[ExerciseInput] {
+
+  override val inputType: InputObjectType[ExerciseInput] = {
+    @unused implicit val x0: InputObjectType[FlatSolutionNodeInput] = FlatSolutionNodeInputGraphQLTypes.inputType
+
+    deriveInputObjectType[ExerciseInput]()
+  }
+
+  // Queries
+
+  private val resolveSampleSolution: Resolver[Exercise, Seq[FlatSampleSolutionNode]] = resolveWithUser { (context, _) =>
+    context.ctx.tableDefs.futureSampleSolutionForExercise(context.value.id)
+  }
+
+  private val resolveAllUserSolutions: Resolver[Exercise, Seq[UserSolution]] = resolveWithCorrector { (context, _) =>
+    context.ctx.tableDefs.futureUserSolutionsForExercise(context.value.id)
+  }
+
+  private val resolveUserSolution: Resolver[Exercise, Option[UserSolution]] = resolveWithCorrector { (context, _) =>
+    context.ctx.tableDefs.futureMaybeUserSolution(context.arg(usernameArg), context.value.id)
+  }
+
+  override val queryType: ObjectType[GraphQLContext, Exercise] = deriveObjectType(
+    AddFields[GraphQLContext, Exercise](
+      Field("sampleSolution", ListType(FlatSampleSolutionNodeGraphQLTypes.queryType), resolve = resolveSampleSolution),
+      Field("userSolutions", ListType(UserSolutionGraphQLTypes.queryType), resolve = resolveAllUserSolutions),
+      Field("userSolution", OptionType(UserSolutionGraphQLTypes.queryType), arguments = usernameArg :: Nil, resolve = resolveUserSolution)
+    )
+  )
+
+  private val resolveSubmitSolution: Resolver[Exercise, Boolean] = resolveWithUser { (context, _) =>
+    @unused implicit val ec: ExecutionContext     = context.ctx.ec
+    val UserSolutionInput(username, flatSolution) = context.arg(userSolutionInputArg)
+
+    for {
+      _ <- context.ctx.tableDefs.futureInsertUserSolutionForExercise(username, context.value.id, flatSolution)
+    } yield true
+  }
+
+  override val mutationType: ObjectType[GraphQLContext, Exercise] = ObjectType(
+    "ExerciseMutations",
+    fields[GraphQLContext, Exercise](
+      Field("submitSolution", BooleanType, arguments = userSolutionInputArg :: Nil, resolve = resolveSubmitSolution),
+      Field("userSolution", OptionType(UserSolutionGraphQLTypes.mutationType), arguments = usernameArg :: Nil, resolve = resolveUserSolution)
+    )
+  )
 
 }
 
