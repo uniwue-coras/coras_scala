f6c9ee5b8844c18aa3f39c128cd2f130dbba1427
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2024-02-14 12:54:14 +0100

    add old cra ui

--- ui/i18next-parser.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  defaultNamespace: 'common',
-  output: 'src/locales/$NAMESPACE_$LOCALE.json',
-  locales: ['de', 'en'],
-  input: [
-    'src/**/*.{js,jsx,ts,tsx}'
-  ]
-};
--- ui/src/icons.tsx
+++
@@ -1,3 +0,0 @@
-import {JSX} from 'react';
-
-export const UpdateIcon: () => JSX.Element = () => <>&#x27F3;</>;
