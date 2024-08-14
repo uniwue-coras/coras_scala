6ae57f8d624e021b68974c966fbdf1dd74c87cb2
Alexander Gehrke <alexander.gehrke@uni-wuerzburg.de>
2024-08-14 17:47:41 +0200

    fix(vite_ui): clean up build and fix test running
    - merge vite.config.js into ts version
    - add jsdom test environment for localStorage
    - update packages

--- vite_ui/vite.config.ts
+++ vite_ui/vite.config.ts
@@ -1,7 +1,20 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+  },
+  server: {
+    port: 5116
+  },
+  plugins: [
+    react(),
+    checker({
+      typescript: true
+    })
+  ]
 });
